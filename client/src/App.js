df1005d52e69288e8c7c74f9f182b82f6cf6eea8
Bret Baker <bretbaker808@gmail.com>
2020-02-05 12:29:46 -0800

    initial commit, basic file cleanup, background img and overlay done

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='body'>
+      <div className='background-overlay'>
+        <h1>Hello World</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
