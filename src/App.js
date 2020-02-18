4fefce16b8f869b7b2332fb729f8c4b8d29ddec7
Bret Baker <bretbaker808@gmail.com>
2020-02-18 10:57:48 -0800

    move redirects back to toml

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ function App() {
                   />
                   <Route exact path='/projects' component={Projects} />
                   <Route exact path='/contact' component={Contact} />
+                  {/* <Route component={} */}
                 </Switch>
               </CSSTransition>
             </TransitionGroup>
